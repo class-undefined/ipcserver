8dd08e3c5c24ada8c39237cda2c72337da38aaf0
野漫横江 <1399226280@qq.com>
2025-01-14 11:09:59 +0800

    xx

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="ipcserver",
-    version="0.2.5",
+    version="0.2.6",
     packages=find_packages(exclude=['tests', 'tests.*']),
     description="A fastapi-like but a sock server",
     install_requires=[
